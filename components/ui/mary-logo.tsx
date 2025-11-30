c29239957baaa1c0e0638b842e3df40d983490dd
Zaza Parkadze <zaza.parkadze@gmail.com>
2025-11-30 16:48:26 +0200

    logo

--- components/ui/mary-logo.tsx
+++ components/ui/mary-logo.tsx
@@ -3,16 +3,14 @@ import { Lato } from "next/font/google";
 
 const lato = Lato({
   subsets: ["latin"],
-  weight: ["400"], //
+  weight: ["400"],
 });
 
 export default function MaryLogo() {
   return (
-    <div>
-      <div className="text-neutral-800  flex items-center justify-center gap-x-1">
-        <Brain className="text-[#a36f6f] size-3.5" />
-        <div className={lato.className}>Mary</div>
-      </div>
+    <div className="text-neutral-800  flex items-center gap-x-1">
+      <Brain className="text-[#a36f6f] size-3.5 pt-px" />
+      <span className={lato.className}>Mary</span>
     </div>
   );
 }
