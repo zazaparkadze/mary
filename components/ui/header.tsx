76265f4c512c423be408a4c09b636dcf716bedd5
Zaza Parkadze <zaza.parkadze@gmail.com>
2025-12-19 22:40:21 +0200

    third

--- components/ui/header.tsx
+++ components/ui/header.tsx
@@ -1,69 +1,33 @@
 "use client";
 import HamburgerButton from "./hamburger-button";
-import SessionButton from "./session-button";
-import ToggleLanguage from "./language-button";
+
 import MaryLogo from "./mary-logo";
 import { useState } from "react";
-import Link from "next/link";
-/* import Navlist from "./navlist"; */
+
+import Navlist from "./navlist";
 
 export default function Header() {
   const [open, setOpen] = useState(false);
+  const cssClassesLg = {
+    divClass: "hidden lg:flex gap-3 items-center",
+    ulClass: "flex gap-3",
+  };
+  const cssClassesSm = {
+    divClass:
+      "group-data-[navlist=false]:hidden  group-data-[navlist=true]:flex flex-col items-center justify-center bg-black/5 backdrop-blur-[3px] animate-mylist origin-top",
+    ulClass: "flex gap-3 flex-col items-center justify-center py-3",
+  };
   return (
     <div className="container flex sticky top-0 w-full justify-between max-w-7xl mx-auto bg-background p-4 lg:p-8 animate-mylist origin-top">
       <div className="grid place-content-center">
         <MaryLogo />
       </div>
-      {/*  <Navlist /> */}
-      <div className="hidden lg:flex gap-3 items-center">
-        <ul className="flex gap-3">
-          <li>About Me</li>
-          <li>Services</li>
-          <li>
-            <a href="#testimonials"> Testimonials</a>
-          </li>
-          <li>
-            <a href="#how-it-works">How it Works</a>
-          </li>
-          <li>
-            <Link href={"blog"}>Blog</Link>
-          </li>
-        </ul>
-        <ToggleLanguage />
-        <SessionButton />
-      </div>
+      <Navlist {...cssClassesLg} />
       <div
         className="group w-full fixed inset-0  mt-15 lg:hidden"
         data-navlist={open ? "true" : "false"}
       >
-        <div
-          className="
-           group-data-[navlist=false]:hidden 
-           group-data-[navlist=true]:flex 
-           flex-col
-           items-center justify-center
-           bg-black/5 backdrop-blur-[3px]
-           animate-mylist origin-top
-           "
-        >
-          <ul className="flex gap-3 flex-col items-center justify-center py-3">
-            <li>About Me</li>
-            <li>Services</li>
-            <li>
-              <a href="#testimonials"> Testimonials</a>
-            </li>
-            <li>
-              <a href="#how-it-works">How it Works</a>
-            </li>
-            <li>
-              <Link href={"blog"}>Blog</Link>
-            </li>
-            <li className="flex gap-x-3">
-              <ToggleLanguage />
-              <SessionButton />
-            </li>
-          </ul>
-        </div>
+        <Navlist {...cssClassesSm} />
       </div>
       <HamburgerButton
         className="lg:hidden"
--- components/ui/navlist.tsx
+++ components/ui/navlist.tsx
@@ -1,20 +1,32 @@
 import Link from "next/link";
+import SessionButton from "./session-button";
+import ToggleLanguage from "./language-button";
 
-export default function Navlist(addClasses: { addClasses?: string }) {
-  const cssClasses = "flex gap-3" + addClasses;
+type cssClasses = {
+  divClass: string;
+  ulClass: string;
+};
+
+export default function Navlist({ divClass, ulClass }: cssClasses) {
   return (
-    <ul className={cssClasses}>
-      <li>About Me</li>
-      <li>Services</li>
-      <li>
-        <a href="#testimonials"> Testimonials</a>
-      </li>
-      <li>
-        <a href="#how-it-works">How it Works</a>
-      </li>
-      <li>
-        <Link href={"blog"}>Blog</Link>
-      </li>
-    </ul>
+    <div className={`${divClass}`}>
+      <ul className={`${ulClass}`}>
+        <li>About Me</li>
+        <li>Services</li>
+        <li>
+          <a href="#testimonials"> Testimonials</a>
+        </li>
+        <li>
+          <a href="#how-it-works">How it Works</a>
+        </li>
+        <li>
+          <Link href={"blog"}>Blog</Link>
+        </li>
+      </ul>
+      <div className="flex gap-3">
+        <ToggleLanguage />
+        <SessionButton />
+      </div>
+    </div>
   );
 }
