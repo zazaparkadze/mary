ff8f91d6d41f056784636bcdbc1734ca0eedddbb
Zaza Parkadze <zaza.parkadze@gmail.com>
2025-12-20 19:26:01 +0200

    bilingual

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -2,7 +2,11 @@ import Header from "@/components/ui/header";
 import Footer from "@/components/ui/footer";
 import Content from "@/components/ui/content";
 
-export default async function Home({ params }: { params: Params }) {
+export default async function Home({
+  params,
+}: {
+  params: Promise<{ locale: Locale }>;
+}) {
   const { locale } = await params;
   return (
     <div className="flex flex-col min-h-screen items-center justify-center font-sans dark:bg-black">
