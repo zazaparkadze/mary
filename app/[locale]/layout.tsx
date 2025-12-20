f86e14eaf48db55b85aa345c14d7dc2676bdb41b
Zaza Parkadze <zaza.parkadze@gmail.com>
2025-12-20 19:35:54 +0200

    bilingual

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -22,7 +22,7 @@ export default async function LocaleLayout({
   params,
 }: {
   children: React.ReactNode;
-  params: Promise<{ locale: Locale }>;
+  params: Promise<{ locale: string | Locale }>;
 }) {
   const { locale } = await params;
   const isRTL = locale === "en";
