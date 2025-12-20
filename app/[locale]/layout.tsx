71ec355479a4fa803a1d1bde79c43eda9307cdac
Zaza Parkadze <zaza.parkadze@gmail.com>
2025-12-20 20:29:19 +0200

    bilingual

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,16 +1,4 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "../globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "doula",
@@ -28,12 +16,8 @@ export default async function LocaleLayout({
   const locale = localeParams.locale as "en" | "he";
   const isRTL = locale === "en";
   return (
-    <html lang={locale} dir={!isRTL ? "rtl" : "ltr"}>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+    <div lang={locale} dir={!isRTL ? "rtl" : "ltr"}>
+      {children}
+    </div>
   );
 }
