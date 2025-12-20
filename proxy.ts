7d1e1d559db47e3d6603dee46a2f467b2d47e155
Zaza Parkadze <zaza.parkadze@gmail.com>
2025-12-20 18:32:06 +0200

    bilingual

--- proxy.ts
+++ proxy.ts
@@ -1,4 +1,5 @@
-import { NextResponse, NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
 export function proxy(request: NextRequest) {
   const { pathname } = request.nextUrl;
